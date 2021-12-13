472b855ab7a1c8329f108e85416f9a4ee729c5e5
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2021-12-14 01:00:31 +0900

    valuable

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ var eq = {
   freq:0,
   coefs:[]
 }
+var creatures=[];
 $("#generateButton").on("click",function(){ //generatebutton is clicked
   eq.freq=Math.floor(Math.random()*10)+1;
-  for(var i = 0;i < eq.freq;i++){
+  for(var i = 0;i < eq.freq;i++){ //-5 <= coef < 5
     eq.coefs[i] = Math.floor(Math.random()*10-5);
   }
   eq.coefs[eq.freq]=1;
   let resTxt="";
-  for(var i = eq.freq;i >=0;i--){
+  for(var i = eq.freq;i >=0;i--){ //generate equation text
     if (eq.coefs[i]) {
       if(i-eq.freq) {
         if (eq.coefs[i]>0) resTxt+="+";
@@ -19,9 +20,20 @@ $("#generateButton").on("click",function(){ //generatebutton is clicked
       if(i) resTxt+="x^"+i;
     }
   }
-  document.getElementById("generatedEq").textContent=resTxt;
+  document.getElementById("generatedEq").textContent=resTxt; //show equation text
 });
 
 $("gaButton").on("click",function(){ //calbuttion is clicked
-
+  const genMax=100; //maximum number of generations
+  const creMax=100; //maximum number of creatures in a generation
+  for(var i = 0;i < genMax;i++){
+    creatures[i]=[];
+    for(var j = 0;j < creMax;j++){
+      creatures[i][j]={ //initialize creatures
+        genes:0,
+        val:0,
+        ancestors:""
+      }
+    }
+  }
 });
